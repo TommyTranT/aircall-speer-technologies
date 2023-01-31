8adcc862d5c3362612eebe381b4a3f9e6e2abca9
Tommy Tran <haitran1995@gmail.com>
2023-01-31 09:07:38 -0500

    import ActivityFeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ActivityFeed from "./components/ActivityFeed.jsx";
 
-import Header from './Header.jsx';
+import Header from "./components/Header.jsx";
 
+const BASE_URL = "https://cerulean-marlin-wig.cyclic.app/";
 const App = () => {
+  const [activities, setActivities] = useState([]);
+
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      <ActivityFeed activities={activities} setActivities={setActivities} />
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
